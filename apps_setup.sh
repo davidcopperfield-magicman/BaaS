e6c63489051bb39047d35b8a2c199f116e67bf73
davidcopperfield-magicman <38091702+davidcopperfield-magicman@users.noreply.github.com>
2018-04-04 22:04:32 -0500

    Update apps_setup.sh

--- apps_setup.sh
+++ apps_setup.sh
@@ -47,7 +47,7 @@ su -l $username -c  "multichain-cli "$chainname" subscribe contract_invited_sign
 ## INSTALLING & CONFIGURING APPS
 ###
 cd $webserver_activeDirectory
-git clone https://github.com/unibitlabs/apps.git
+git clone https://github.com/davidcopperfield-magicman/VoteApp.git
 
 # Configuring Yobiapps
 sudo sed -ie 's/$CHAIN_NAME =.*;/$CHAIN_NAME = "'$chainname'";/g' $webserver_activeDirectory/apps/config.php
